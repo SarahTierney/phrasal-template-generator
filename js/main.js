1083ae3cb90ca2fc971647eca76144399b78ed0b
SarahTierney <tierney.s@live.com>
2016-03-03 22:56:59 -0500

    Fixed inconsistent type

--- js/main.js
+++ js/main.js
@@ -4,30 +4,34 @@ var adjective;
 var verb;
 var story;
 
-alert('Lets make a story together!');
+var a;
+var b;
+var c;
 
-color = prompt('A colour (orange, indigo, etc.)')
-creature = prompt('A creature (dragon, moth, etc.)')
-adjective = prompt('An adjective (beautiful, super, etc.)')
-verb = prompt('A past tense verb (ran, burped, etc.)')
-story = prompt('Which story would you like? \nEnter the letter below: \na) Snacking \nb) Escaping \nc) Winning')
+alert('Let’s make a story together!');
+
+color = prompt('A color (orange, indigo, etc.)');
+creature = prompt('A creature (dragon, moth, etc.)');
+adjective = prompt('An adjective (beautiful, super, etc.)');
+verb = prompt('A past tense verb (ran, burped, etc.)');
+story = prompt('Which story would you like? \nEnter the letter below: \na) Snacking \nb) Escaping \nc) Winning');
 
 a = function () {
-  document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
-}
+  document.write('After snacking on ' + adjective + ' treats, the ' + color + ' bellied ' + creature + (' ') + verb + ' for hours.');
+};
 
 b = function () {
-  document.write('Ameilia ' + verbed + ' through the ' + adjective + (' ') + colour + ' nebula escaping the space ' + creature + '.');
-}
+  document.write('Ameilia ' + verb + ' through the ' + adjective + (' ') + color + ' nebula escaping the space ' + creature + '.');
+};
 
-c = function () {
-  document.write('Jackson chose his ' + adjective + ', ' + colour + (' ') + creature + ' card and ' + verbed + ' it to the table knowing he won.');
-}
+c = function ()  {
+  document.write('Jackson chose his ' + adjective + ', ' + color + (' ') + creature + ' card and ' + verb + ' it to the table knowing he won.');
+};
 
 if (story == 'a' || story == 'A') {
   a();
 } else if (story == 'b' || story == 'B') {
   b();
-} else if (story == 'c' || story =='C') {
+} else if (story == 'c' || story == 'C') {
   c();
 }
