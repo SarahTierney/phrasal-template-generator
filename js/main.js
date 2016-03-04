20b842cdb3cff4f20705cae7fa0db9d4cea567d5
SarahTierney <tierney.s@live.com>
2016-03-03 21:36:24 -0500

    Made prompts

--- js/main.js
+++ js/main.js
@@ -3,3 +3,11 @@ var creature;
 var adjective;
 var verb;
 var story;
+
+alert('Lets make a story together!');
+
+color = promt('A colour (orange, indigo, etc.)')
+creature = promt('A creature (dragon, moth, etc.)')
+adjective = promt('An adjective (beautiful, super, etc.)')
+verb = promt('A past tense verb (ran, burped, etc.)')
+story = promt('')
