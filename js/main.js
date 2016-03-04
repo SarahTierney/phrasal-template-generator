352f36e3845310f628123a36ff29f88e0b9875ec
SarahTierney <tierney.s@live.com>
2016-03-03 22:12:52 -0500

    Added "if" part

--- js/main.js
+++ js/main.js
@@ -12,14 +12,22 @@ adjective = promt('An adjective (beautiful, super, etc.)')
 verb = promt('A past tense verb (ran, burped, etc.)')
 story = promt('Which story would you like? \nEnter the letter below: \na) Snacking \nb) Escaping \nc) Winning')
 
-if (story == 'a') {
-  document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' + verb + ' for hours.');
-};
+a = function () {
+  document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
+}
 
-if (story == 'b') {
-  document.write('Ameilia ' + verbed + ' through the " + adjective + " ' + colour + ' nebula escaping the space ' + creature + '.');
-};
+b = function () {
+  document.write('Ameilia ' + verbed + ' through the ' + adjective + (' ') + colour + ' nebula escaping the space ' + creature + '.');
+}
 
-if (story == 'c') {
-  document.write('Jackson chose his ' + adjective + ', ' + colour + ' ' + creature + ' card and ' + verbed + ' it to the table knowing he won.');
-};
+c = function () {
+  document.write('Jackson chose his ' + adjective + ', ' + colour + (' ') + creature + ' card and ' + verbed + ' it to the table knowing he won.');
+}
+
+if (story == 'a' || story == 'A') {
+  a();
+} else if (story == 'b' || story == 'B') {
+  b();
+} else if (story == 'c' || story =='C') {
+  c();
+}
