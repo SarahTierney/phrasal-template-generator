b8e0cde18e806fb62ebba7efc090f9540f416bf3
SarahTierney <tierney.s@live.com>
2016-03-03 21:58:17 -0500

    Added "if" statements

--- js/main.js
+++ js/main.js
@@ -10,4 +10,16 @@ color = promt('A colour (orange, indigo, etc.)')
 creature = promt('A creature (dragon, moth, etc.)')
 adjective = promt('An adjective (beautiful, super, etc.)')
 verb = promt('A past tense verb (ran, burped, etc.)')
-story = promt('')
+story = promt('Which story would you like? \nEnter the letter below: \na) Snacking \nb) Escaping \nc) Winning')
+
+if (story == 'a') {
+  document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' + verb + ' for hours.')
+}
+
+if (story == 'b') {
+  document.write('Ameilia ' + verbed + ' through the " + adjective + " ' + colour + ' nebula escaping the space ' + creature + '.')
+}
+
+if (story == 'c') {
+  document.write('Jackson chose his ' + adjective + ', ' + colour + ' ' + creature + ' card and ' + verbed + ' it to the table knowing he won.')
+}
